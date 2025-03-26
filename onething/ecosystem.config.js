b31f6a11806e1598bf6edc26672be53318d2493c
chenping <chenping@tailup.love>
2025-03-25 23:10:54 -0700

    deploy.yml 成功运行6-添加Node_env环境变量配置和写入

---
+++ onething/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+      name: 'onething',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }]
+  };
